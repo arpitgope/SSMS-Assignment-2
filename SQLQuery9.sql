3e0c8593ec5da2f59c5037cc6877804bbf52fcb3
arpitgope <146990380+arpitgope@users.noreply.github.com>
2025-06-14 13:26:37 +0530

    Delete SQLQuery9.sql

--- SQLQuery9.sql
+++
@@ -1,7 +0,0 @@
--- Should succeed
-INSERT INTO OrderDetails (OrderID, ProductID, Quantity)
-VALUES (1, 1, 10);
-
--- Should fail due to insufficient stock
-INSERT INTO OrderDetails (OrderID, ProductID, Quantity)
-VALUES (1, 1, 1000);
